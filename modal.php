40829f3c3a6a24f962c58d190c8566ec84201978
ozermert26 <ozermert26@gmail.com>
2021-02-23 12:45:53 +0300

    Abi lütfen modal.php dosyasını oku

---
+++ modal.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Kapatma denemesi</h1>
+        <p>Şuan bir kapatma denemesi yapıyorum.</p>
+            <p>Aşağıdaki paragrafı kapatmak için kapat düğmesine basın.</p>
+
+            <div id="kapatma-paragraf">
+<p>Bu paragrafı kapat düğmesine basarak kapatamıyorum.!</p>
+        <button name="kapat" onclick="kapat()">Kapat</button>
+    </div>
+<script>
+function kapat(){
+var a = document.getElementByİd('kapatma-paragraf');
+<.style="display: none;"
+}
+</script>
+
+</body>
+</html>
